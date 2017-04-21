c120e07e7b4690d4383f4cd2d877fb2fbfad919b
Matthew J. Mucklo <mmucklo@gmail.com>
2017-04-21 01:03:18 -0700

    work in progress splitting out English inflections, culling from Doctrine's Inflector, rails, wikipedia, and the original mmucklo/inflect library

---
+++ src/En/Plural.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace src\Inflections\En;
+
+/**
+ * English Plural Inflections
+ *   See https://en.wikipedia.org/wiki/English_plurals
+ *   See https://github.com/doctrine/inflector/blob/master/lib/Doctrine/Common/Inflector/Inflector.php.
+ */
+class Plural
+{
+    public static $plural = [
+        '/(s)tatus$/i' => '\1\2tatuses',
+        '/(quiz)$/i' => '\1zes',
+        '/^(ox)$/i' => '\1en',
+        '/([m|l])ouse$/i' => '\1ice',
+        '/(matr|vert|ind)(ix|ex)$/i' => '\1ices',
+        '/(x|ch|ss|sh)$/i' => '\1es',
+        '/([^aeiouy]|qu)y$/i' => '\1ies',
+        '/(hive)$/i' => '\1s',
+        '/(?:([^f])fe|([lr])f)$/i' => '\1\2ves',
+        '/(shea|lea|loa|thie)f$/i' => '\1ves',
+        '/sis$/i' => 'ses',
+        '/([ti])um$/i' => '\1a',
+        '/(p)erson$/i' => '\1eople',
+        '/(m)an$/i' => '\1en',
+        '/(c)hild$/i' => '\1hildren',
+        '/(f)oot$/i' => '\1eet',
+        '/(buffal|ech|her|potat|tomat|volcan|vet)o$/i' => '\1\2oes',
+        '/(alumn|bacill|cact|foc|fung|nucle|radi|stimul|syllab|termin|vir)us$/i' => '\1i',
+        '/us$/i' => 'uses',
+        '/(alias)$/i' => '\1es',
+        '/(analys|ax|cris|test|thes)is$/i' => '\1es',
+        '/s$/' => 's',
+        '/^$/' => '',
+        '/$/' => 's', ];
+
+    public static $uninflected = [
+        '.*[nrlm]ese',
+        '.*deer',
+        '.*fish',
+        '.*measles',
+        '.*ois',
+        '.*pox',
+        '.*sheep',
+        'people',
+        'cookie', ];
+
+    public static $irregular = [
+        'automaton' => 'automata',
+        'atlas' => 'atlases',
+        'axe' => 'axes',
+        'beef' => 'beefs',
+        'brother' => 'brothers',
+        'cafe' => 'cafes',
+        'cello' => 'celli',
+        'chateau' => 'chateaux',
+        'cherub' => 'cherubim',
+        'child' => 'children',
+        'cookie' => 'cookies',
+        'cow' => 'cows',
+        'criterion' => 'criteria',
+        'corrigendum' => 'corrigenda',
+        'curriculum' => 'curricula',
+        'demo' => 'demos',
+        'domino' => 'dominoes',
+        'dormouse' => 'dormice',
+        'ganglion' => 'ganglions',
+        'genie' => 'genies',
+        'genus' => 'genera',
+        'goose' => 'geese',
+        'graffito' => 'graffiti',
+        'hippopotamus' => 'hippopotami',
+        'hoof' => 'hoofs',
+        'human' => 'humans',
+        'iris' => 'irises',
+        'man' => 'men',
+        'medium' => 'media',
+        'memorandum' => 'memoranda',
+        'money' => 'monies',
+        'mongoose' => 'mongooses',
+        'motto' => 'mottoes',
+        'move' => 'moves',
+        'mythos' => 'mythoi',
+        'niche' => 'niches',
+        'numen' => 'numina',
+        'occiput' => 'occiputs',
+        'octopus' => 'octopuses',
+        'opus' => 'opuses',
+        'ovum' => 'ova',
+        'ox' => 'oxen',
+        'person' => 'people',
+        'plateau' => 'plateaux',
+        'phalanx' => 'phalanges',
+        'phenomenon' => 'phenomena',
+        'polyhedron' => 'polyhedra',
+        'refferendum' => 'refferendums', // https://www.merriam-webster.com/dictionary/referendum
+        'runner-up' => 'runners-up',
+        'seraph' => 'seraphim',
+        'sex' => 'sexes',
+        'soliloquy' => 'soliloquies',
+        'son-in-law' => 'sons-in-law',
+        'tooth' => 'teeth',
+        'tornado' => 'tornadoes',
+        'trilby' => 'trilbys',
+        'turf' => 'turfs',
+        'viscus' => 'vicera',
+        'volcano' => 'volcanoes',
+        'woman' => 'women',
+        'zombie' => 'zombies', ];
+}
---
+++ src/En/Singular.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Inflections\En;
+
+/**
+ * English Plural Inflections
+ *   See https://github.com/doctrine/inflector/blob/master/lib/Doctrine/Common/Inflector/Inflector.php.
+ */
+class Singular
+{
+    public static $rules = [
+        '/(s)tatuses$/i' => '\1\2tatus',
+        '/^(.*)(menu)s$/i' => '\1\2',
+        '/(quiz)zes$/i' => '\\1',
+        '/(matr)ices$/i' => '\1ix',
+        '/(vert|ind)ices$/i' => '\1ex',
+        '/^(ox)en/i' => '\1',
+        '/(alias)(es)*$/i' => '\1',
+        '/(buffal|her|potat|tomat|volcan)oes$/i' => '\1o',
+        '/(alumn|bacill|cact|foc|fung|nucle|radi|stimul|syllab|termin|viri?)i$/i' => '\1us',
+        '/([ftw]ax)es/i' => '\1',
+        '/(analys|ax|cris|test|thes)es$/i' => '\1is',
+        '/(shoe|slave)s$/i' => '\1',
+        '/(o)es$/i' => '\1',
+        '/ouses$/' => 'ouse',
+        '/([^a])uses$/' => '\1us',
+        '/([m|l])ice$/i' => '\1ouse',
+        '/(x|ch|ss|sh)es$/i' => '\1',
+        '/(m)ovies$/i' => '\1\2ovie',
+        '/(s)eries$/i' => '\1\2eries',
+        '/([^aeiouy]|qu)ies$/i' => '\1y',
+        '/([lr])ves$/i' => '\1f',
+        '/(tive)s$/i' => '\1',
+        '/(hive)s$/i' => '\1',
+        '/(drive)s$/i' => '\1',
+        '/([^fo])ves$/i' => '\1fe',
+        '/(^analy)ses$/i' => '\1sis',
+        '/(analy|diagno|^ba|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\1\2sis',
+        '/([ti])a$/i' => '\1um',
+        '/(p)eople$/i' => '\1\2erson',
+        '/(m)en$/i' => '\1an',
+        '/(c)hildren$/i' => '\1\2hild',
+        '/(f)eet$/i' => '\1oot',
+        '/(n)ews$/i' => '\1\2ews',
+        '/eaus$/' => 'eau',
+        '/^(.*us)$/' => '\\1',
+        '/s$/i' => '', ];
+
+    public static $uninflected = [
+        '.*[nrlm]ese',
+        '.*deer',
+        '.*fish',
+        '.*measles',
+        '.*ois',
+        '.*pox',
+        '.*sheep',
+        '.*ss',
+    ];
+
+    public static $irregular = [
+        'criteria' => 'criterion',
+        'curves' => 'curve',
+        'emphases' => 'emphasis',
+        'foes' => 'foe',
+        'hoaxes' => 'hoax',
+        'media' => 'medium',
+        'neuroses' => 'neurosis',
+        'waves' => 'wave',
+        'oases' => 'oasis',
+    ];
+}
